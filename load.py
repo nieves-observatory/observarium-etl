db9310f2018d1b0184535130b8eb6cd591de43f3
Jerrick <weejerrick@gmail.com>
2021-10-02 10:54:04 +0800

    Add ETL of Caldwell catalogue

--- load.py
+++ load.py
@@ -1,13 +1,10 @@
 from flask import Flask  # to render the error page
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost/dso_without_c13"
 db = SQLAlchemy(app)
 
 
@@ -31,16 +28,11 @@ db.create_all()
 print("create model success")
 
 
-t_host = os.getenv("t_host ")
-t_port = os.getenv("t_port")
-t_dbname = os.getenv("t_dbname")
-t_user = os.getenv("t_user")
-t_pw = os.getenv("t_pw")
-db_conn = psycopg2.connect(host=t_host, port=t_port,
-                           dbname=t_dbname, user=t_user, password=t_pw)
+db_conn = psycopg2.connect(host="localhost", port="5432",
+                           dbname="dso_without_c13", user="postgres")
 db_cursor = db_conn.cursor()
 print("connection success")
-f_contents = open('complete_messier.csv', 'r')
+f_contents = open('dso_complete.csv', 'r')
 
 db_cursor.copy_from(f_contents, "dso", columns=("params", 'name', 'ngcic', 'common', 'type', 'ra', 'dec', 'mag', 'dist', 'con',
                                                 'image'), sep=",")
